b9ec94e5202f9682fd7fd257512774f686f6640b
lorreshi <lorre_shi@163.com>
2025-07-22 01:39:55 +0800

    feat(Log):add Logger and LogAppender class
    新增日志类以及抽象接口Appender作为观察者模式输出日志

--- include/log.h
+++ include/log.h
@@ -5,19 +5,18 @@
 #ifndef LOG_H
 #define LOG_H
 #include <cstdint>
-#include <string>
+#include <list>
 #include <memory>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <strstream>
+#include <iostream>
 
 namespace sly {
     class LogLevel {
     public:
-        enum Level {
-            DEBUG = 0,
-            INFO,
-            WARNING,
-            ERROR,
-            FATAL
-        };
+        enum Level { DEBUG = 0, INFO, WARNING, ERROR, FATAL };
     };
 
     class LogEvent {
@@ -42,7 +41,7 @@ namespace sly {
     public:
         typedef std::shared_ptr<LogFormatter> ptr;
 
-        std::string format(const LogEvent &event);
+        std::string format(LogEvent::ptr event);
 
     private:
     };
@@ -54,8 +53,18 @@ namespace sly {
 
         virtual ~LogAppender();
 
-    private:
+        virtual void log(LogLevel::Level level, const LogEvent::ptr event) = 0;
+
+        void setFormatter(LogFormatter::ptr formatter) {
+            m_formatter = formatter;
+        }
+        LogFormatter::ptr getFormatter() const {
+            return m_formatter;
+        }
+
+    protected:
         LogLevel::Level m_level;
+        LogFormatter::ptr m_formatter;
     };
 
     /* 日志器 */
@@ -63,30 +72,57 @@ namespace sly {
     public:
         Logger(const std::string &name = "root");
 
-        ~Logger();
-
         void log(LogLevel::Level level, const LogEvent::ptr event);
 
+        void debug(LogEvent::ptr event);
+        void info(LogEvent::ptr event);
+        void warn(LogEvent::ptr event);
+        void error(LogEvent::ptr event);
+        void fatal(LogEvent::ptr event);
+
+        void addAppender(LogAppender::ptr appender);
+        void delAppender(LogAppender::ptr appender);
+
+        LogLevel::Level getLevel() const { return m_level; }
+
+        void setLevel(LogLevel::Level level) { m_level = level; }
+
     private:
+        std::string m_name;
+        LogLevel::Level m_level;
+        std::list<LogAppender::ptr> m_appender;
     };
 
     /* 输出到控制台
      * 设置为final函数，禁止此class被继承。
      */
     class StdoutLogAppender final : public LogAppender {
+    public:
+        typedef std::shared_ptr<StdoutLogAppender> ptr;
+        void log(LogLevel::Level level, const LogEvent::ptr event) override;
+
+    private:
     };
 
     /* 输出到文件 */
     class FileLogAppender final : public LogAppender {
+    public:
+        typedef std::shared_ptr<FileLogAppender> ptr;
+        FileLogAppender(const std::string &filename);
+        void log(LogLevel::Level level, const LogEvent::ptr event) override;
+
+        bool reopen();
+
+    private:
+        std::string m_filename;
+        std::ofstream m_filestream;
     };
 
     /* 输出到网络服务器 */
-    class NetworkLogAppender final : public LogAppender {
-    };
+    class NetworkLogAppender final : public LogAppender {};
 
     /* 输出到数据库 */
-    class DataLogAppender final : public LogAppender {
-    };
-}
+    class DataLogAppender final : public LogAppender {};
+} // namespace sly
 
-#endif //LOG_H
+#endif // LOG_H
--- src/log.cc
+++ src/log.cc
@@ -2,3 +2,64 @@
 // Created by 石立宇 on 25-7-22.
 //
 #include "log.h"
+
+namespace sly {
+
+    Logger::Logger(const std::string &name) : m_name(name) {}
+
+    void Logger::addAppender(LogAppender::ptr appender) {
+        m_appender.push_back(appender);
+    }
+
+    void Logger::delAppender(LogAppender::ptr appender) {
+        for (auto it = m_appender.begin(); it != m_appender.end(); ++it) {
+            if (*it == appender) {
+                m_appender.erase(it);
+                break;
+            }
+        }
+    }
+
+    void Logger::log(LogLevel::Level level, const LogEvent::ptr event) {
+        if (level < m_level) return;
+        for (auto& i : m_appender) {
+            i->log(level, event);
+        }
+    }
+
+    void Logger::debug(LogEvent::ptr event) {
+        log(LogLevel::DEBUG, event);
+    }
+    void Logger::info(LogEvent::ptr event) {
+        log(LogLevel::INFO, event);
+    }
+    void Logger::warn(LogEvent::ptr event) {
+        log(LogLevel::WARNING, event);
+    }
+    void Logger::error(LogEvent::ptr event) {
+        log(LogLevel::ERROR, event);
+    }
+    void Logger::fatal(LogEvent::ptr event) {
+        log(LogLevel::FATAL, event);
+    }
+
+    void StdoutLogAppender::log(LogLevel::Level level, const LogEvent::ptr event) {
+        if (level < m_level) return;
+        std::cout << m_formatter->format(event);
+    }
+
+    void FileLogAppender::log(LogLevel::Level level, const LogEvent::ptr event) {
+        if (level < m_level) return;
+        m_filestream << m_formatter->format(event);
+    }
+
+    bool FileLogAppender::reopen() {
+        if (m_filestream.is_open()) {
+            m_filestream.close();
+        }
+        m_filestream.open(m_filename);
+        return m_filestream.is_open();
+    }
+
+
+} // namespace sly
